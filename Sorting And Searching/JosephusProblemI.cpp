8412ad63f2270621a607fcfeecc62d31b8bcd80c
sauram <ss0274307@gmail.com>
2021-10-04 19:42:29 +0530

    Added Documentation

--- Sorting And Searching/JosephusProblemI.cpp
+++ Sorting And Searching/JosephusProblemI.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <queue>
 using namespace std;
+/*
+    This is solved using different method.
+*/
 int main(){
     int number;
-    cin>> number;
+    cin >> number;
     queue <int> numberqueue;
     for (int i = 1; i <= number; i++){
         numberqueue.push(i);
     }
-    int j=1;
+    int j = 1;
     while(!numberqueue.empty()){
         if(j&1){
             int temp = numberqueue.front();
